4aef42138e665a1a72123d9d1be5b7b99f8dc46b
GDJ <GDJ@DESKTOP-1US9OA7>
2023-05-22 17:26:27 +0900

    프론트,서블릿 사이에 요청,응답

--- 02_servletdata/src/main/java/com/servlet/controller/EnrollMemberServlet.java
+++ 02_servletdata/src/main/java/com/servlet/controller/EnrollMemberServlet.java
@@ -17,6 +17,7 @@ public class EnrollMemberServlet extends HttpServlet{
 	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8"); //html파일의 인코딩 값과 동일하게 적용해야한다.
 		Map<String,String[]> param=req.getParameterMap();
 		for(String key:param.keySet()) {
 			System.out.println(key+" : "+Arrays.toString(param.get(key)));
@@ -24,6 +25,12 @@ public class EnrollMemberServlet extends HttpServlet{
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.doGet(req, resp);
+		//보낸 방식에 차이가 있어서 메소드가 나뉠뿐 해당 데이터 처리하는 로직이 같다면 doGet메소드를 불러와서 이용할 수 있다.
+		//post는 바이트로 쪼개서 전송하기 때문에 그대로 받아올 경우 영문자,숫자 외에는 전부 깨지게 된다.
+		//때문에 post방식으로 데이터를 받는 경우 인코딩 처리 해줘야한다.
+		//HttpServletRequest.setCharacterEncoding()을 이용한다.
+		
 	}
 	
 
